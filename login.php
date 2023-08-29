c36604975faa10ce923738bffac2b8d836cceaac
carterkruse <carterjkruse@gmail.com>
2023-08-29 09:28:48 -0400

    updated php login page

--- login.php
+++ login.php
@@ -16,7 +16,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     if (empty($username)) {
         header("Location: index.php?error=Username is required!");
         exit();
-
     } else if (empty($password)) {
         header("Location: index.php?error=Password is required!");
         exit();
